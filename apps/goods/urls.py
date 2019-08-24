f78f2fd65e802e9adf084c27b493a3b59e86fd4a
too-hoo <13414851554@163.com>
2019-08-24 19:36:09 +0800

    商品列表页内容的获取和显示、列表页页码的控制

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
-from apps.goods.views import indexView, DetailView
+from apps.goods.views import indexView, DetailView, ListView
 
 
 urlpatterns = [
     # 加上index的时候只是为了在部署的时候加以区分
     path('',indexView.as_view(), name='index'),  # 显示首页
     re_path(r'^goods/(?P<goods_id>\d+)$', DetailView.as_view(), name='detail'), # 详情页面,获取id,数字,一个或者多个
+    re_path(r'^list/(?P<type_id>\d+)/(?P<page>\d+)$', ListView.as_view(), name='list'), # 列表页面
 ]
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import View
 from django.core.cache import cache
+from django.core.paginator import Paginator  # Django里面的分页模块
 from apps.goods.models import GoodsType, GoodsSKU, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
 from django_redis import get_redis_connection
 from apps.order.models import OrderGoods
@@ -81,7 +83,7 @@ class DetailView(View):
             sku = GoodsSKU.objects.get(id=goods_id)
         except GoodsSKU.DoesNotExist:
             # 商品不存在,就返回首页
-            return redirect(reversed('goods:index'))
+            return redirect(reverse('goods:index'))
 
         # 获取商品的分类信息
         types = GoodsType.objects.all()
@@ -108,7 +110,7 @@ class DetailView(View):
 
             # 添加用户的历史记录, 记录保存到列表里面去
             conn = get_redis_connection('default')
-            history_key = 'history_%d'%user.id
+            history_key = 'history_%d' % user.id
             # 移除列表中的goods_id, 0表示移除所有相等的值
             conn.lrem(history_key, 0, goods_id)
             # 把goods_id 插入到列表的左侧
@@ -120,7 +122,95 @@ class DetailView(View):
         context = {'sku': sku, 'types': types,
                    'sku_orders': sku_orders,
                    'new_skus': new_skus,
-                   'same_spu_skus':same_spu_skus,
+                   'same_spu_skus': same_spu_skus,
                    'cart_count': cart_count}
 
         return render(request, 'detail.html', context)
+
+
+# 种类id & 页码 & 排序的方式
+# restful api -> 请求一种资源
+# /list?type_id=种类id&page=页码&sort=排序方式
+# /list/种类id/页码/排序方式
+
+# /list/种类id/页码?sort=排序方式  <--
+class ListView(View):
+    '''列表页'''
+
+    def get(self, request, type_id, page):
+        '''显示列表页'''
+        # 对type_id进行校验
+        try:
+            type = GoodsType.objects.get(id=type_id)
+        except GoodsType.DoesNotExist:
+            # 种类不存在
+            return redirect(reverse('goods:index'))
+
+        # 获取商品的分类信息
+        types = GoodsType.objects.all()
+
+        # 获取排序方式 获取分类商品的信息
+        # sort=default, 按照默认的id排序;sort=price,按照价格; sort=hot, 按照销量;
+        sort = request.GET.get('sort')
+
+        if sort == 'price':
+            skus = GoodsSKU.objects.filter(type=type).order_by('price')
+        elif sort == 'hot':
+            skus = GoodsSKU.objects.filter(type=type).order_by('-sales')
+        else:
+            # 最后要赋值为Default,否则sort=None
+            sort = 'default'
+            skus = GoodsSKU.objects.filter(type=type).order_by('-id')
+
+        # 对数据进行分页处理,这里设置每页显示几条数据,例如每页显示2条
+        paginator = Paginator(skus, 1)
+
+        try:
+            page = int(page)
+        except Exception as e:
+            page = 1
+        # 使用paginator 内置的模块总页数来限定查询的页数
+        if page > paginator.num_pages or page <= 0:
+            page = 1
+
+        # 获取第page页的Page实例对象
+        skus_page = paginator.page(page)
+
+        # TODO:进行页码的控制.每页最多显示5个页码
+        # 1.总页数小于5页,页面上显示所有的页码
+        # 2.如果当前页是前3页,显示1-5页
+        # 3.如果当前页是后3页,显示后五页
+        # 4.其他情况,显示当前页的前两页,当前页,当前页的后五页
+        num_pages = paginator.num_pages
+        if num_pages < 5:
+            pages = range(1, num_pages + 1)
+        elif page <= 3:
+            pages = range(1, 6)
+        elif num_pages - page <= 2: # 10 9 8 相差2, 后-前 得到相差页数
+            pages = range(num_pages - 4, num_pages + 1)
+        else:
+            pages = range(page - 2, page + 3)
+
+        # 获取新品信息
+        new_skus = GoodsSKU.objects.filter(type=type).order_by('-create_time')
+
+        # 获取登录用户的额购物车中的商品的数量
+        user = request.user
+        cart_count = 0
+        if user.is_authenticated:
+            # 用户已经登录
+            conn = get_redis_connection('default')
+            cart_key = 'cart_%d' % user.id
+
+            # 获取用户购物车中的商品条目数
+            cart_count = conn.hlen(cart_key)  # hlen hash中的数目
+
+        # 组织模板上下文
+        context = {'type': type, 'types': types,
+                   'sort': sort,
+                   'skus_page': skus_page,
+                   'new_skus': new_skus,
+                   'pages': pages,
+                   'cart_count': cart_count}
+        # 使用模板
+        return render(request, 'list.html', context)
